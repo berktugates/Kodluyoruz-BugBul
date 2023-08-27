3012f3a63e263833bbbb79f962806604035d34b6
Batuhan Erol <bataer57@gmail.com>
2023-08-27 13:41:47 +0300

    questions' solutions has been added

---
+++ Oda_4_Java/src/MoveZeros.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+// https://leetcode.com/problems/move-zeroes/description/
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int counter = 0;
+        for(int i = nums.length-1; i>=0;i--){
+            if(nums[i]!=0){
+                stack.push(nums[i]);
+                counter++;
+            }
+        }
+        int sizeForZero = nums.length - counter;
+        int j = 0;
+        while(counter>0){
+            nums[j++] = stack.pop();
+            counter--;
+        }
+        while(sizeForZero>0){
+            nums[j++] = 0;
+            sizeForZero--;
+        }
+    }
+}
